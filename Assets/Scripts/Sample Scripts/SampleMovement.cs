6689d3e306737a07e54edfebf6688516c4d23ee1
AReeme <areeme@student.neumont.edu>
2023-11-09 09:58:58 -0700

    Endless Runner Basis Created

---
+++ Assets/Scripts/Sample Scripts/SampleMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform GFX;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform feetPos;
+    [SerializeField] private float groundDistance = 0.25f;
+    [SerializeField] private float jumpTime = 0.3f;
+
+    [SerializeField] private float crouchHeight = 0.5f;
+
+    private bool isGrounded = false;
+    private bool isJumping = false;
+    private float jumpTimer;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, groundDistance, groundLayer);
+
+        #region Jumping
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (isJumping && Input.GetButtonDown("Jump"))
+        {
+            if (jumpTimer < jumpTime)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+
+                jumpTimer += Time.deltaTime;
+            } else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = false;
+            jumpTimer = 0;
+        }
+        #endregion
+
+        #region Crouching
+
+        if (isGrounded && Input.GetButton("Crouch"))
+        {
+            GFX.localScale = new Vector3(GFX.localScale.x, crouchHeight, GFX.localScale.z);
+
+            if (isJumping)
+            {
+                GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
+            }
+        }
+
+        if (Input.GetButtonUp("Crouch"))
+        {
+            GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
+        }
+
+        #endregion
+    }
+}
