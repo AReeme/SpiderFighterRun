b741105cfe3bc033e029ece3641584c0e7733ff6
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2023-11-09 09:59:32 -0700

    Shop In Progress

---
+++ Assets/Scripts/Shop/ButtonInfo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonInfo : MonoBehaviour
+{
+
+    public int ItemID;
+    public TMPro.TextMeshPro Price;
+    public TMPro.TextMeshPro Quantity;
+    public GameObject ShopManager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
