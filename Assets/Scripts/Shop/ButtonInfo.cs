0f9a0104e4ab5254bb9ac9ea60a70eab2e745249
epiccreeper10 <87875284+epiccreeper10@users.noreply.github.com>
2023-11-16 10:00:21 -0700

    Prevent shop issues

--- Assets/Scripts/Shop/ButtonInfo.cs
+++ Assets/Scripts/Shop/ButtonInfo.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ButtonInfo : MonoBehaviour
 {
@@ -15,7 +16,17 @@ public class ButtonInfo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Price.text = "Price: " + ShopManager.GetComponent<ShopManagerScript>().dataPersistence.shopItemsData[2, ItemID].ToString();
-        Quantity.text = ShopManager.GetComponent<ShopManagerScript>().dataPersistence.shopItemsData[3, ItemID].ToString();
+        try
+        {
+            Price.text = "Price: " + ShopManager.GetComponent<ShopManagerScript>().dataPersistence.shopItemsData[2, ItemID].ToString();
+            Quantity.text = ShopManager.GetComponent<ShopManagerScript>().dataPersistence.shopItemsData[3, ItemID].ToString();
+        }
+        catch (System.Exception ex)
+        {
+            // Log the error for debugging purposes
+            Debug.Log("Error occurred in ShopManager: Do not load shop by itself");
+            // Load Scene 2
+            SceneManager.LoadScene("Scene 2");
+        }
     }
 }
