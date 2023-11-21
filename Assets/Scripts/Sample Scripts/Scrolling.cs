4df825ca479adb9cbf91aa908b150f0acec8665e
AReeme <areeme@student.neumont.edu>
2023-11-21 09:16:02 -0700

    Scrolling Background Implemented

---
+++ Assets/Scripts/Sample Scripts/Scrolling.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scrolling : MonoBehaviour
+{
+    [SerializeField] RawImage _img;
+    [SerializeField] private float _x, _y;
+    public GameManager gm;
+    int scoreThreshold = 50;
+    public List<Texture> backgroundTextures;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x, _y) * Time.deltaTime, _img.uvRect.size);
+
+        if (gm.currentScore > scoreThreshold)
+        {
+            ChangeBackground();
+        }
+    }
+
+    void ChangeBackground()
+    {
+        int backgroundIndex = Mathf.FloorToInt(gm.currentScore / scoreThreshold) % backgroundTextures.Count;
+        _img.texture = backgroundTextures[backgroundIndex];
+    }
+}
